ffc575600d7234dfa8ca7935c0e9ccd0fe2da6f4
sboomi <47070729+sboomi@users.noreply.github.com>
2021-01-11 00:16:00 +0100

    Added web app

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -11,7 +11,10 @@ cb = ChatbotModel('flaskapp/utils/models/chatbot_model', 'flaskapp/utils/data/in
 @app.route('/message', methods=["POST"])
 def message():
     message_content = json.loads(request.data)
+    
     response = cb.generate_response(message_content['data'], as_dict=True)
+    app.logger.info(f"Data: {message_content}, response: {response}")
+
     return jsonify(response)
 
 if __name__ == '__main__':
--- web/index.js
+++ web/index.js
@@ -1,2 +1,55 @@
 var chatMessages = [];
 
+let btn = document.getElementById('sendButton')
+
+btn.addEventListener("click", function() {
+    let msg = document.getElementById("userMessage").value
+    var xhr = new XMLHttpRequest();
+    // xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+    document.getElementById("testUserMsg").innerHTML = msg;
+    
+    xhr.open('POST', 'http://localhost:5000/message');
+    let test = {"data": msg}
+    // send the request with the song
+    let dataString = JSON.stringify(test);
+    xhr.timeout = 10000;
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+        var jsonData = JSON.parse(xhr.responseText);
+        if (xhr.status === 200) {
+            console.log('successful');
+            let response = jsonData.response;
+            document.getElementById("testBotMsg").innerHTML = response;  
+            } else {
+            console.log('failed');
+            document.getElementById("testBotMsg").innerHTML = "I'm sorry there was a request error";  
+            }
+        }
+    };
+
+    xhr.send(dataString);
+
+
+    
+
+    //Receive response
+    xhr.addEventListener("load", function() {
+        let response = "";
+        if(xhr.status == 200) {
+            let result_api = JSON.parse(xhr.responseText);
+            response = result_api.response; 
+            document.getElementById("testBotMsg").innerHTML = response;  
+        } else {
+            response = "No response available"
+            alert(`${xhr.status}: ${xhr.statusText}`);
+        }
+    });
+
+    alert(`Your response is ${response}`);
+
+    document.getElementById("userMessage").value = "";
+
+    
+    
+})
